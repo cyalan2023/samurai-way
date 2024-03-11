429411a2f411ce4148b4fd6def932bf19bb2f346
Alex <cyalan2019@gmail.com>
2024-03-11 16:06:26 +0500

    readme

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://w7.pngwing.com/pngs/1020/508/png-transparent-google-chrome-computer-icons-web-browser-desktop-chrome.png"
+                alt="#"/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://s1.1zoom.ru/b5050/115/Tropics_Coast_Branches_481275_3840x2400.jpg" alt=""/>
+            </div>
+            <div>
+                ava + desctription
+            </div>
+            <div>
+                my posts
+                <div>
+                    newpost
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+
