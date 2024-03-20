67dbbe2e667e7ac8763c244a55c4c593720459a7
Alex <cyalan2019@gmail.com>
2024-03-20 13:07:58 +0500

    readme

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://s1.1zoom.ru/b5050/115/Tropics_Coast_Branches_481275_3840x2400.jpg" alt=""/>
             </div>
